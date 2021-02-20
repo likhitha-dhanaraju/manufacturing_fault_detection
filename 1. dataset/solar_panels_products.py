57a195332b6c59aaf50e74c24e900bcbbf21a74d
likhitha_d01 <likhitha.d01@gmail.com>
2021-02-21 00:26:05 +0530

    fixed solar panels preprocessing

--- 1. dataset/solar_panels_products.py
+++ 1. dataset/solar_panels_products.py
@@ -26,10 +26,12 @@ if not os.path.exists(destination_folder):
 
 images, _, types = load_dataset(datafolder = 'elpv_dataset')
 
+idx = 0
 
-for (idx, (image ,type_)) in tqdm(enumerate(zip(images, types))):
+for image ,type_ in zip(images, types):
 
-	image = cv2.resize(image, (300, 300)).reshape( image.shape[0] * image.shape[1])
+
+	image = cv2.resize(image, (300, 300)).reshape(300,300,1)
 
 	category_folder = os.path.join(destination_folder, type_)
 
@@ -38,7 +40,10 @@ for (idx, (image ,type_)) in tqdm(enumerate(zip(images, types))):
 
 	image_name = os.path.join( category_folder, 'elpv_' + str(idx) + '.jpg')
 
+
 	if not os.path.exists(image_name):
 
-		cv2.imwrite( image_name, image )
+		cv2.imwrite(image, image_name )
+
+	idx += 1
 	
